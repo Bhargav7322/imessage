c03e7c3f26ee8140ec2420b50daefbda26905105
Bhargav7322 <bhargav.meva@gmail.com>
2026-07-02 11:00:41 +0530

    Add Log And Sound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-
-import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
 function App() {
 
   return (
     <>
-    <h1>App</h1>
+    <h1 className="text-4xl text-red-500 bg-blue-400">App</h1>
+    
+    <Button>mu button</Button>
     <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
