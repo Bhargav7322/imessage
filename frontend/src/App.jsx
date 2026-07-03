2480da8d5fb1fba6abfb156c03d431e14535179f
Bhargav7322 <bhargav.meva@gmail.com>
2026-07-03 16:36:29 +0530

    Add Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import { ThemeProvider } from './context/ThemeContext';
 import { Navigate, Route, Routes } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
 
 const {isSignedIn, isLoaded} = useAuth();
 
-  return (
+if (!isLoaded) return <div>Loading...</div>
+ 
+return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
         
         </Routes>
       </WallpaperProvider>
