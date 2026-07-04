0eda52d95da230f09b27c94a3bc98034e2734f32
Bhargav7322 <bhargav.meva@gmail.com>
2026-07-04 16:01:25 +0530

    Add Zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,57 @@
-
-import { WallpaperProvider } from './context/WallPaperContext';
-import { ThemeProvider } from './context/ThemeContext';
-import { Navigate, Route, Routes } from 'react-router';
-import ChatPage from './pages/ChatPage';
-import AuthPage from './pages/AuthPage';
-import { useAuth } from '@clerk/react';
-import PageLoader from './components/auth/PageLoader';
+import { WallpaperProvider } from "./context/WallPaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import PageLoader from "./components/auth/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
 
-const {isSignedIn, isLoaded} = useAuth();
+  if (!isLoaded) return <PageLoader />;
 
-if (!isLoaded) return <PageLoader />;
- 
-return (
+  // this is one also working perfectly this is simple distucturing and work correctly but this is not optimized way to use zustand store
+  // option 1
+  // const {checkAuth,isCheckingAuth,clearAuth} = useAuthStore();
+
+  //option2 - better for performance
+  // if this way use so more optimized way to use zustand store and better performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isSignedIn, isLoaded]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
+  return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
-        
+          <Route
+            path="/"
+            element={
+              isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
+            }
+          />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
